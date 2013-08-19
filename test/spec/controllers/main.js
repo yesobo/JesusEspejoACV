539b713dab675dbfe150908221e7c78cb05951f5
yesobo <yeso40@hotmail.com>
2013-08-20 00:37:51 +0200

    2 test failing

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,6 +1,7 @@
+/* global describe, beforeEach, it, expect, inject:false */
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: ExperienceCtrl', function () {
 
   // load the controller's module
   beforeEach(module('JesusEspejoACVApp'));
@@ -11,24 +12,46 @@ describe('Controller: MainCtrl', function () {
   // creating a mock service
   beforeEach(module(function($provide) {
     $provide.factory('mockSharedData', function() {
-      var getSharedData = function(callBack){
-        return callBack([
-          {
-            'client': {
-              'name': 'Metro de Madrid S.A.',
-              'imageUrl': 'img/client/metro_madrid_logo.png'
-            }
-          },
-          {
-            'client': {
-              'name': 'Bankia',
-              'imageUrl': 'img/client/bankia_logo.png'
-            }
+      var getSharedData = function() {
+        
+        return {
+          query:function(callBack){
+            return callBack([
+              {
+                'client': {
+                  'name': 'Metro de Madrid S.A.',
+                  'imageUrl': 'img/client/metro_madrid_logo.png'
+                }
+              },
+              {
+                'client': {
+                  'name': 'Bankia',
+                  'imageUrl': 'img/client/bankia_logo.png'
+                }
+              }
+            ]);
           }
-        ]);
+        };
       };
+
+      var getEmployersLastDate = function() {
+        var expectedResult = [];
+        expectedResult.Employer1 = {
+          start: '2009-06-01 00:00:00 UTC',
+          end: '2011-04-01 00:00:00 UTC'
+        };
+        
+        expectedResult.Employer2 = {
+          start: '2009-06-01 00:00:00 UTC',
+          end: '2011-04-01 00:00:00 UTC'
+        };
+
+        return expectedResult;
+      };
+
       return {
-        getSharedData: getSharedData
+        getSharedData: getSharedData,
+        getEmployersLastDate: getEmployersLastDate
       };
     });
   }));
@@ -36,7 +59,7 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, mockSharedData) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {$scope: scope, sharedData: mockSharedData});
+    MainCtrl = $controller('ExperienceCtrl', {$scope: scope, SharedData: mockSharedData});
   }));
 
   it('should create "employment" model with 2 employments fetched from xhr', function() {
