8ee0f866888a66bf7dedaae73dc2a23145f930c5
yesobo <JEspejo@ATJESPEJOP.atsistemas.lan>
2013-08-23 14:20:18 +0200

    e2e tests passed

--- test/e2e/experienceView.js
+++ test/e2e/experienceView.js
@@ -14,9 +14,10 @@ describe('Experience View', function() {
   });
 
   it('should filter the employments list as user types into the search box', function() {
-    expect(repeater('.employment').count()).toBe(5);
+    expect(repeater('.employment').count()).toBe(7);
 
     input('query').enter('Bankia');
+    
     expect(repeater('.employment').count()).toBe(1);
 
     input('query').enter('2012');
@@ -25,12 +26,6 @@ describe('Experience View', function() {
 
   it('should order the employments list by descending starting date', function() {
     expect(element('.employment:first .positionTitle').text()).toContain("Bankia");
-  });
-
-  it('should render employment specific links', function() {
-    input('query').enter('bankia');
-    element('.employment header a').click();
-    expect(browser().location().url()).toBe('/experience/Bankia');
   });
 });
 
