490a804ae0b42683973b5d7b9418bff6091d4d38
yesobo <yeso40@hotmail.com>
2013-09-16 20:34:01 +0200

    period count added

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -10,4 +10,32 @@ angular.module('JesusEspejoACVApp')
 			}
 			return angularDateFilter(d.getTime(), 'MM-yyyy');
     };
-  });
+})
+.filter('datesDiff', function($filter) {
+
+	return function(datesObj) {
+		var dStart = new Date(datesObj[0]);
+		var dEnd = new Date(datesObj[1]);
+		var month_diff = (dEnd.getFullYear() - 
+			dStart.getFullYear()) * 12 + dEnd.getMonth() - dStart.getMonth();
+		var years = Math.floor(month_diff / 12);
+		var months = month_diff % 12;
+		var result = "";
+		if ( years > 0 ) {
+			result += years + ' year';
+			if ( years > 1) {
+				result += 's';
+			}
+			if ( months > 0) {
+				result += ' ';
+			}
+		}
+		if ( months > 0 ) {
+			result += months + ' month';
+			if (months > 1) {
+				result += 's';
+			}
+		}
+		return result;
+	};
+});
