b06cb601f492cb06b94f2b40c59f107e96b651c6
Jesus Espejo <yeso40@hotmail.com>
2014-08-07 15:26:19 +0200

    error when adding collapseScrollTop on isolated scope of panelCollapseButton directive

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,56 +1,58 @@
 'use strict';
 
 angular.module('JesusEspejoACVControllers', ['pascalprecht.translate'])
-  .controller('ExperienceCtrl',
-    ['$scope', '$window', 'SharedData', '$translate', '$parse',
-    function (sc, window, SharedData, translate) {
+    .controller('ExperienceCtrl', ['$scope', '$window', 'SharedData', '$translate', '$parse',
+        function(sc, window, SharedData, translate) {
 
-      var NO_FOCUS_SEARCH = 0;
+            var NO_FOCUS_SEARCH = 0;
 
-      var employments = sc.employments =
-        SharedData.getSharedDataResource().query(function() {
-        sc.employersDates =
-          SharedData.getEmployersPeriods(employments);
-      });
+            var employments = sc.employments =
+                SharedData.getSharedDataResource().query(function() {
+                    sc.employersDates =
+                        SharedData.getEmployersPeriods(employments);
+                });
 
-      sc.activateSearchButton = true;
-
-      sc.buttonContainerClass = '';
-      sc.sectionTitleWrapperVisible = 'true';
-      sc.searchButtonInputFocus = 'false';
-
-      sc.searchButtonPlaceholder = translate('SEARCH');
-
-      sc.hideAndExpand = function(mode) {
-        if(window.innerWidth <= 480 ) {
-          if(sc.activateSearchButton) {
-            sc.activateSearchButton = false;
-            if(mode === NO_FOCUS_SEARCH) {
-              sc.sectionTitleWrapperVisible = 'true';
-            } else {
-              sc.sectionTitleWrapperVisible = 'false';
-              sc.searchButtonInputFocus = 'true';
-            }
-          }
-        }
-      };
-
-      sc.exitSearchMode = function() {
-        if(window.innerWidth <= 480 ) {
-          if(!sc.query || sc.query === '') {
-            sc.sectionTitleWrapperVisible = 'true';
             sc.activateSearchButton = true;
-          }
+
+            sc.buttonContainerClass = '';
+            sc.sectionTitleWrapperVisible = 'true';
+            sc.searchButtonInputFocus = 'false';
+
+            sc.collapseScrollTop = 0;
+
+            sc.searchButtonPlaceholder = translate('SEARCH');
+
+            sc.hideAndExpand = function(mode) {
+                if (window.innerWidth <= 480) {
+                    if (sc.activateSearchButton) {
+                        sc.activateSearchButton = false;
+                        if (mode === NO_FOCUS_SEARCH) {
+                            sc.sectionTitleWrapperVisible = 'true';
+                        } else {
+                            sc.sectionTitleWrapperVisible = 'false';
+                            sc.searchButtonInputFocus = 'true';
+                        }
+                    }
+                }
+            };
+
+            sc.exitSearchMode = function() {
+                if (window.innerWidth <= 480) {
+                    if (!sc.query || sc.query === '') {
+                        sc.sectionTitleWrapperVisible = 'true';
+                        sc.activateSearchButton = true;
+                    }
+                }
+            };
         }
-      };
-    }])
-  .controller('ExperienceDetailCtrl',
-    ['$scope', '$routeParams', 'SharedData',
-    function ($scope, $routeParams, SharedData) {
-      var allExperiences = SharedData.getSharedData()
-        .query( function() {
-        $scope.experience =
-            SharedData.filterJSON(allExperiences, 'start',
-              $routeParams.start)[0];
-      });
-    }]);
+    ])
+    .controller('ExperienceDetailCtrl', ['$scope', '$routeParams', 'SharedData',
+        function($scope, $routeParams, SharedData) {
+            var allExperiences = SharedData.getSharedData()
+                .query(function() {
+                    $scope.experience =
+                        SharedData.filterJSON(allExperiences, 'start',
+                            $routeParams.start)[0];
+                });
+        }
+    ]);
--- app/scripts/directives/panelCollapseButton.js
+++ app/scripts/directives/panelCollapseButton.js
@@ -3,31 +3,43 @@
 'use strict';
 
 angular.module('PanelCollapseButtonDirective', [])
-.directive('jeaCollapseButton', function() {
+    .directive('jeaCollapseButton', function() {
 
-  function makeSticky(jElement, jParent) {
-    jElement.stick_in_parent({
-        offset_top: 50,
-        parent: jParent,
-      });
-  }
+        function makeSticky(jElement, jParent) {
+            jElement.stick_in_parent({
+                offset_top: 50,
+                parent: jParent,
+            }).on("sticky_kit:stick", function(e) {
+                console.log('scroll pos: ' + $(document).scrollTop());
+                collapseScrollTop = $(document).scrollTop();
+            }).on("sticky_kit:unstick", function(e) {
+                console.log('unstuck');
+            });
+        }
 
-  return {
-    restrict: 'A',
-    link: function(scope, elem) {
+        function unbindSticky(jElement) {
+            jElement.off("sticky_kit:stick").off("sticky_kit:unstick");
+        }
 
-      var jPanel = $(elem[0].parentElement.parentElement);
-      var stickyZone = jPanel.children('.stickyZone');
-      var panelHeader = stickyZone.children('.panel-heading');
-      jPanel.on('shown.bs.collapse', function () {
-          makeSticky(panelHeader, stickyZone);
-          $(document.body).trigger('sticky_kit:recalc');
-        });
-      jPanel.on('hidden.bs.collapse', function () {
-          $(document.body).trigger('sticky_kit:recalc');
-          // commented because of sticky-kit issue #23
-          //panelHeader.trigger('sticky_kit:detach');
-        });
-    }
-  };
-});
+        return {
+            restrict: 'A',
+            // TODO: add collapseScrollTop to isolated scope
+            scope: true,
+            link: function(scope, elem) {
+                var jPanel = $(elem[0].parentElement.parentElement);
+                var stickyZone = jPanel.children('.stickyZone');
+                var panelHeader = stickyZone.children('.panel-heading');
+                jPanel.on('shown.bs.collapse', function() {
+                    makeSticky(panelHeader, stickyZone);
+                    $(document.body).trigger('sticky_kit:recalc');
+                });
+                jPanel.on('hidden.bs.collapse', function() {
+                    unbindSticky(panelHeader);
+                    //window.scrollTo(0, collapseScrollTop);
+                    $(document.body).trigger('sticky_kit:recalc');
+                    // commented because of sticky-kit issue #23
+                    //panelHeader.trigger('sticky_kit:detach');
+                });
+            }
+        };
+    });
